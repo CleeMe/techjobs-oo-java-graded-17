3c63d5d0d0a7caa2326febb6d5376358da3e7e99
Christi-Lee <christileecurrier@gmail.com>
2023-09-27 15:37:02 -0500

    Task 4: test the equals method

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,6 +1,7 @@
 package org.launchcode.techjobs.oo;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import org.junit.Test;
@@ -30,5 +31,19 @@ public class JobTest {
         assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
 
     }
+    @Test
+    public void testJobsForEquality() {
+
+        Job job1 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        Job job2 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertFalse(job1.equals(job2));
+    }
+
 
 }
