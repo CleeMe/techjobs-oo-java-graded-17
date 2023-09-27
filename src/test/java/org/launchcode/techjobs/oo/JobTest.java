4754e87c210e3fcf0f8a3f3f011cadcdb80809b8
Christi-Lee <christileecurrier@gmail.com>
2023-09-27 15:13:54 -0500

    Task 4 tested assertNotEquals

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,17 @@
 package org.launchcode.techjobs.oo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.junit.Test;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        assertNotEquals(job1.getId(), job2.getId());
+    }
 }
