83e0e905e1851c5625c2da219950caeba9cb7e5e
Christi-Lee <christileecurrier@gmail.com>
2023-10-03 19:00:19 -0500

    created JobField.java and compiled the shared fields etc.  and edited Employer.java to extend JobField

--- src/main/java/org/launchcode/techjobs/oo/JobField.java
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -12,4 +12,37 @@ public class JobField {
         id = nextId;
         nextId++;
     }
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField jobField = (JobField) o;
+        return getId() == jobField.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
